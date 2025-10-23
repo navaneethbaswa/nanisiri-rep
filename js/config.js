84f7d76711a5881815fb010f55e58d7ab3db4356
navaneethbaswa <162604536+navaneethbaswa@users.noreply.github.com>
2025-10-23 16:25:50 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_pzlEuNrS5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7fe3pcmf9t206tr0ls3a2sd726', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3deo96d32sqm4msdh6djfak4fb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
